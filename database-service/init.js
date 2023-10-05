db99397d91d1a639a63b408bf90ccbe977d13a8c
fatima-aamir <aamirfatima350@gmail.com>
2023-10-05 15:00:26 +0500

    updated readme, init.js, dockerfile

--- database-service/init.js
+++ database-service/init.js
@@ -1,16 +1,7 @@
-// init.js
-// Script to initialize MongoDB collections and insert data
-
-// Import the MongoDB Node.js driver
 const { MongoClient } = require('mongodb');
-
-// MongoDB connection URL
 const url = 'mongodb://localhost:27017';
 
-// Database name
 const dbName = 'my_database';
-
-// MongoDB client
 const client = new MongoClient(url);
 
 // Data initialization function
