8ff1f79849f2a42448e0eb23c3ec835488ca4bad
fatima-aamir <aamirfatima350@gmail.com>
2023-10-08 13:14:41 +0500

    updated the flask code to connect with db

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from pymongo import MongoClient
+import datetime
 
 app = Flask(__name__)
 app.secret_key = "secret_key"  # Change this to a random secret key
+
+app = Flask(__name__)
+app.secret_key = "2378F98"  # Change this to a random secret key
+
+# MongoDB connection
+client = MongoClient('mongodb://localhost:27017')
+db = client['my_database']
+users_collection = db['usersCollection']
 
 @app.route('/')
 def root():
@@ -22,6 +32,12 @@ def login():
         username = request.form['username']
         password = request.form['password']
         # You should add code here to check users credentials (use a database in a real app)
+        user_data = {
+            'username': username,
+            'login_time': datetime.datetime.now()  # Import datetime module
+        }
+        users_collection.insert_one(user_data)
+
         session['username'] = username
         return redirect(url_for('welcome'))
     return render_template('login.html')
@@ -34,5 +50,5 @@ def welcome():
     return redirect(url_for('login'))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
 
